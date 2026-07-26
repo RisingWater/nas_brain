b309a3bf07e5102e2a4625aaeba3a58ecd4310fc
risingwater <4680414@qq.com>
2026-07-26 21:34:41 +0800

    Revert "fix: _play_audio is_active 加5s超时防卡死"
    This reverts commit 78500cf29e29c0b08fedfbfbcc41051335eb33e2.

--- src/gateways/voice/processor.py
+++ src/gateways/voice/processor.py
@@ -128,9 +128,8 @@ class VoiceProcessor:
                 stream.write(pcm[offset:end])
                 offset = end
             
-            # 等待缓冲区排空（最多等 5 秒）
-            wait_start = time.time()
-            while stream.is_active() and time.time() - wait_start < 5:
+            # 等待缓冲区排空
+            while stream.is_active():
                 time.sleep(0.01)
             stream.stop_stream()
                 
